850589dcbf1365185fa5cff580bf624f70e1d7b5
Tara Whirley <whirleyt@outlook.com>
2018-01-17 16:53:09 -0500

    Completed

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
-const express = require( 'express' );
-const app = express(); 
 
-app.listen(3000, function(){
-    console.log("server listening")
-})
+'use strict';
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+var makesRouter = require('./routes');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var bodyParser = require('body-parser');
+var socketio = require('socket.io');
 
-app.use(function (req, res, next) {
-    console.log('request', req.method)
-    next()
-    // do your logging here
-    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-})
+// templating boilerplate setup
+app.engine('html', nunjucks.render); // how to render html templates
+app.set('view engine', 'html'); // what file extension do our templates have
++nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
 
-app.get('/', function(req, res){
-    res.send("Hello World!");
-    console.log('We are tracking requests', req.method, '/', res.statusCode)
+// logging middleware
+app.use(morgan('dev'));
 
-})
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
 
-app.get('/news', function(req, res){
-    res.send("This is news!");
-})
 
+// start the server
+var server = app.listen(1337, function(){
+  console.log('listening on port 1337');
+});
+var io = socketio.listen(server);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+// modular routing that uses io inside it
+app.use('/', makesRouter(io));
+
+// // manually-written static file middleware
+// app.use(function(req, res, next){
+//   var mimeType = mime.lookup(req.path);
+//   fs.readFile('./public' + req.path, function(err, fileBuffer){
+//     if (err) return next();
+//     res.header('Content-Type', mimeType);
+//     res.send(fileBuffer);
+//   });
+// });
